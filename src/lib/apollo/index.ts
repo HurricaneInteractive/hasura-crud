ce9b62ea75aa8b65a8b35e977fb7b9cac6e60686
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-09-28 19:59:40 +1000

    feat: config apollo

---
+++ src/lib/apollo/index.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'https://apparent-oarfish-93.hasura.app/v1/graphql',
+  cache: new InMemoryCache()
+});
